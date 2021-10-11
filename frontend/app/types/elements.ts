0c88ac8b4dc3187af476f9401b880b131b96bec3
Rizky Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2021-10-11 12:54:37 +0700

    create home page template

--- frontend/app/types/elements.ts
+++ frontend/app/types/elements.ts
@@ -2,3 +2,12 @@ export enum colorChoices {
   Primary = "primary",
   Secondary = "secondary",
 }
+
+export type Service = {
+  id: number;
+  name: string;
+  thumbnail: string;
+  short_description: string;
+  long_description: string;
+  slug: string;
+};
