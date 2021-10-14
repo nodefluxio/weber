4051d96592312367665ee508e7541cd9e9d21cfa
sonadztux <dev.sonadztux@gmail.com>
2021-10-14 16:02:59 +0700

    Update VisitorActivity model & add CreateVisitorActivity()

--- backend/models/visitor_activity.go
+++ backend/models/visitor_activity.go
@@ -2,12 +2,25 @@ package models
 
 import (
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type VisitorActivity struct {
-	ID        string `gorm:"primary_key"`
-	SessionID string
-	ServiceID int
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        	uint 		`gorm:"primaryKey; autoIncrement"`
+	// `VisitorActivity` belongs to `Visitor`, `SessionID` is the foreign key
+	SessionID 	string
+	Visitor		Visitor		`gorm:"foreignKey:SessionID"`
+	// `VisitorActivity` belongs to `Service`, `ServiceID` is the foreign key
+	ServiceID 	uint
+	Service		Service		`gorm:"foreignKey:ServiceID"`
+	CreatedAt 	time.Time
+}
+
+func CreateVisitorActivity(db *gorm.DB, VisitorActivity *VisitorActivity) (err error) {
+	err = db.Create(VisitorActivity).Error
+	if err != nil {
+		return err
+	}
+	return nil
 }
