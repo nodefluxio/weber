b2d8c7d572836c222460d5c9ec093a2f5b222112
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-10-14 11:27:49 +0700

    add Modal element

---
+++ frontend/app/components/elements/Modal/Modal.tsx
@@ -0,0 +1,44 @@
+import React, { ReactNode, useEffect, useRef, useState } from 'react'
+import ReactDOM from 'react-dom'
+import styles from './Modal.module.scss'
+type Props = {
+  show: Boolean
+  onClose: () => void
+  children: ReactNode
+}
+
+export const Modal = ({ show, onClose, children }: Props) => {
+  const [isBrowser, setIsBrowser] = useState(false)
+
+  useEffect(() => {
+    setIsBrowser(true)
+  }, [])
+
+  const handleCloseClick = (e: any) => {
+    console.log(e)
+    e.preventDefault()
+    onClose()
+  }
+
+  const modalContent = show ? (
+    <div className={styles.overlay}>
+      <div className={styles.modal}>
+        <div className={styles.header}>
+          <a href='#' onClick={handleCloseClick}>
+            x
+          </a>
+        </div>
+        <div className={styles.body}>{children}</div>
+      </div>
+    </div>
+  ) : null
+
+  if (isBrowser) {
+    return ReactDOM.createPortal(
+      modalContent,
+      document.getElementById('modal-root')!
+    )
+  } else {
+    return null
+  }
+}
