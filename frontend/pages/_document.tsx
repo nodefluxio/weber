db0376c5f2d697f7f2115bf06d8a22d3b92a1a35
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-10-14 11:27:01 +0700

    add _document file to able to use modal

---
+++ frontend/pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
+
+class MainDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <div id='modal-root'></div>
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MainDocument
