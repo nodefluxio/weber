f491c90b1e45c0e509dc22b3df7cdd30078291ae
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-10-19 14:20:16 +0700

    add content and style to  RequestDemoPage

--- frontend/app/components/templates/RequestDemoPage/RequestDemoPage.tsx
+++ frontend/app/components/templates/RequestDemoPage/RequestDemoPage.tsx
@@ -5,12 +5,26 @@ export const RequestDemoPage = () => {
   const router = useRouter()
 
   return (
-    <div className={styles.container}>
-      <div>
-        <h1>Request Nodeflux Demo</h1>
-      </div>
-      <div>
-        <RequestDemoForm onSuccess={() => router.push('/')} />
+    <div className="sectionContainer">
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <h1>Try our demo in your browser</h1>
+          <p>
+            If you are interested in working with us or just want to talk shop
+            with some really smart, talented people, let us know! Fill out the
+            form below and one of our team members will get back to you right
+            away.
+          </p>
+        </div>
+        <div className={styles.formContent}>
+          <div className={styles.formCard}>
+            <p className={styles.formText}>
+              Help us with some basic information so that we can better cater to
+              your business needs
+            </p>
+            <RequestDemoForm onSuccess={() => router.push('/')} />
+          </div>
+        </div>
       </div>
     </div>
   )
