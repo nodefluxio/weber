c2395effd3be1be085c75d097fd7a1d2d3f78f82
William Fu <williamfu37@gmail.com>
2021-10-25 12:11:00 +0700

    fix: getstaticprops on analytics and api call

--- frontend/pages/analytics/ocr-ktp.tsx
+++ frontend/pages/analytics/ocr-ktp.tsx
@@ -1,41 +1,42 @@
-import type { NextPage } from 'next'
-import {
-  ServiceByIdResponse
-} from '../../app/types/responses'
 import AnalyticsPage from '../../app/components/templates/AnalyticsPage/AnalyticsPage'
-import { useEffect, useState } from 'react'
 import { getServiceById } from '../../app/api/analyticsAPI'
-const OCR: NextPage = () => {
 
-  const [data, setData] = useState<ServiceByIdResponse["data"]>()
-  const [error, setError] = useState<string>()
+interface Props {
+  name: string,
+  short_description: string,
+  long_description: string
+}
 
-  useEffect(() => {
-    getServiceById(1)
-      .then((res: ServiceByIdResponse | undefined) => {
-        setData(res!.data)
-      })
-      .catch((err) => {
-        setError((err as Error).message)
-      })
-  }, [])
+const OCR_ANALYTICS_ID = 1
 
-  if (error) return <div>An error happened, return to homepage</div>
-  if (!data) return <div>Please wait. . .</div>
-  else
-    return (
-      <AnalyticsPage
-        analyticsName={data.name}
-        shortDescription={data.short_description}
-        longDescription={data.long_description}
-        examples={[
-          '/assets/images/ktp1.jpg',
-          '/assets/images/ktp2.jpeg',
-          '/assets/images/ktp3.jpg'
-        ]}
-        serviceID={1}
-      />
-    )
+export const getStaticProps = async () => {
+  try {
+    const res = await getServiceById(OCR_ANALYTICS_ID)
+    return {
+      props: {
+        ...res?.data
+      }
+    }
+  } catch (e) {
+    return (e as Error).message
+  }
+}
+
+const OCR: React.FC<Props> = ({ name, short_description, long_description }) => {
+
+  return (
+    <AnalyticsPage
+      analyticsName={name}
+      shortDescription={short_description}
+      longDescription={long_description}
+      examples={[
+        '/assets/images/ktp1.jpg',
+        '/assets/images/ktp2.jpeg',
+        '/assets/images/ktp3.jpg'
+      ]}
+      serviceID={OCR_ANALYTICS_ID}
+    />
+  )
 }
 
 export default OCR
