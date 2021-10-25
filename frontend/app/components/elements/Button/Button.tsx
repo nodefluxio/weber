c764d14e584df572ee033c530b7ffdd78184db05
Rizky Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2021-10-25 23:15:32 +0700

    add button disabled props

--- frontend/app/components/elements/Button/Button.tsx
+++ frontend/app/components/elements/Button/Button.tsx
@@ -8,11 +8,12 @@ type Props = {
   type?: 'button' | 'submit' | 'reset' | 'link' | undefined
   rounded?: boolean
   onClick?: MouseEventHandler<HTMLButtonElement>
+  disabled?: boolean
   className?: string
 }
 
 export const Button = forwardRef(
-  ({ children, color, type, rounded, onClick, className }: Props, ref) => {
+  ({ children, color, type, rounded, onClick, className, disabled }: Props, ref) => {
     let tagName = 'button'
     if (type === 'link') {
       tagName = 'a'
@@ -23,6 +24,7 @@ export const Button = forwardRef(
     const attributes = {
       className: `${styles.btn} ${color && styles[color]} ${rounded && styles.rounded} ${className}`,
       type: type === 'link' ? undefined : type,
+      disabled: disabled,
       onClick: onClick,
       ref: ref
     }
