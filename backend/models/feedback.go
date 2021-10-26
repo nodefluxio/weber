d15afb079af9c9ce93b2a642bd59e3af2c0ad7b7
bagasteguhimani <bagasteguhimani10@gmail.com>
2021-10-27 00:05:32 +0700

    update on feedback models

--- backend/models/feedback.go
+++ backend/models/feedback.go
@@ -2,13 +2,23 @@ package models
 
 import (
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type Feedback struct {
-	ID                string `gorm:"primary_key"`
-	VisitorActivityID string
-	Text              string
-	Emoji             string
+	ID                uint `gorm:"primary_key"`
+	VisitorActivityID uint
+	VisitorActivity   VisitorActivity `gorm:"foreignKey:VisitorActivityID"`
+	Rating            uint            `json:"rating" validate:"required,min=1,max=5"`
+	Comment           string          `json:"comment" validate:"required,min=20,max=255"`
 	CreatedAt         time.Time
-	UpdatedAt         time.Time
+}
+
+func CreateFeedbackDb(db *gorm.DB, Feedback *Feedback) (err error) {
+	err = db.Create(Feedback).Error
+	if err != nil {
+		return err
+	}
+	return nil
 }
