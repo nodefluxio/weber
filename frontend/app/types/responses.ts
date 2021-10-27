b4af87da0fe17f33d2c34ab481aa3e814657c662
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-10-27 20:01:10 +0700

    add OCR,LPR,FME result responses

--- frontend/app/types/responses.ts
+++ frontend/app/types/responses.ts
@@ -24,33 +24,33 @@ export type VisitorsPostErrorResponse = {
 
 export type ServiceByIdResponse = {
   data: {
-    id: number,
-    type: string,
-    slug: string,
-    name: string,
-    short_description: string,
-    long_description: string,
-    thumbnail: string,
-    created_at: string,
+    id: number
+    type: string
+    slug: string
+    name: string
+    short_description: string
+    long_description: string
+    thumbnail: string
+    created_at: string
     updated_at: string
-  },
-  message: string,
+  }
+  message: string
   ok: boolean
 }
 
 export type ServiceByIdErrorResponse = {
-  message: string,
+  message: string
   ok: boolean
 }
 
-export type AnalyticsResponse = {
-  message: string,
-  ok: boolean,
+export type AnalyticsResponse<AnalyticsResultResponse> = {
+  message: string
+  ok: boolean
   service_data: {
     job: {
       result: {
-        analytic_type: string,
-        result: Object[],
+        analytic_type: string
+        result: [AnalyticsResultResponse]
         status: string
       }
     }
@@ -58,6 +58,49 @@ export type AnalyticsResponse = {
 }
 
 export type AnalyticsError = {
-  message: string,
+  message: string
   ok: boolean
+}
+
+export type OCRResultResponse = {
+  agama: string
+  alamat: string
+  berlaku_hingga: string
+  golongan_darah: string
+  jenis_kelamin: string
+  kabupaten_kota: string
+  kecamatan: string
+  kelurahan_desa: string
+  kewarganegaraan: string
+  nama: string
+  nik: string
+  pekerjaan: string
+  provinsi: string
+  rt_rw: string
+  status_perkawinan: string
+  tanggal_lahir: string
+  tempat_lahir: string
+}
+
+export type LPRResultResponse = {
+  license_plate_recognitions: [
+    {
+      bounding_box: {
+        height: number
+        left: number
+        top: number
+        width: number
+      }
+      confidence: number
+      detected: boolean
+      license_plate_number: string
+    }
+  ]
+}
+
+export type FMEResultResponse = {
+  face_match: {
+    match: boolean
+    similarity: number
+  }
 }
