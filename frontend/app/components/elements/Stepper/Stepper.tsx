8d15fb448db7c193d5e008b1e610ab5d374e8862
Rizky Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2021-10-28 15:05:13 +0700

    make stepper responsive

--- frontend/app/components/elements/Stepper/Stepper.tsx
+++ frontend/app/components/elements/Stepper/Stepper.tsx
@@ -1,23 +1,27 @@
-import styles from "./Stepper.module.scss"
+import styles from './Stepper.module.scss'
 
 type Props = {
-  steps: string[],
+  steps: string[]
   activeStep: number
 }
 
-export const Stepper = ({steps, activeStep}: Props) => {
+export const Stepper = ({ steps, activeStep }: Props) => {
   return (
     <ol className={styles.stepper}>
-      {
-        steps.map((step: string, idx: number) => (
-          <li
-            className={styles.stepperItem}
-            key={idx}>
-              <div className={`${styles.stepperBulletin} ${ idx+1 === activeStep ? styles.active : styles.inactive }`}/>
-              <p>{ step }</p>
-          </li>
-        ))
-      }
+      {steps.map((step: string, idx: number) => (
+        <li
+          className={`${styles.stepperItem} ${
+            idx + 1 === activeStep ? styles.current : ''
+          } ${idx + 1 === activeStep + 1 ? styles.next : ''}`}
+          key={idx}>
+          <div
+            className={`${styles.stepperBulletin} ${
+              idx + 1 === activeStep ? styles.active : styles.inactive
+            }`}
+          />
+          <p>{step}</p>
+        </li>
+      ))}
     </ol>
   )
 }
