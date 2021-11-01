d4debe3d50b2762be31d1d777fd96292758aec69
Lazuardy Khatulistiwa <dimazkid@gmail.com>
2021-11-01 15:51:04 +0700

    split the drop table method

--- backend/database/migrate.go
+++ backend/database/migrate.go
@@ -7,14 +7,16 @@ import (
 )
 
 func Migrate(db *gorm.DB) {
-	// To clean up previous created records
-	// db.Migrator().DropTable(&models.Visitor{})
-	// db.Migrator().DropTable(&models.Feedback{})
-	// db.Migrator().DropTable(&models.VisitorActivity{})
-	// db.Migrator().DropTable(&models.Service{})
-
 	db.AutoMigrate(&models.Visitor{})
 	db.AutoMigrate(&models.Feedback{})
 	db.AutoMigrate(&models.VisitorActivity{})
 	db.AutoMigrate(&models.Service{})
+}
+
+func DropTable(db *gorm.DB) {
+	// To clean up previous created records
+	db.Migrator().DropTable(&models.Visitor{})
+	db.Migrator().DropTable(&models.Feedback{})
+	db.Migrator().DropTable(&models.VisitorActivity{})
+	db.Migrator().DropTable(&models.Service{})
 }
