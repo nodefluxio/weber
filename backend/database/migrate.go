0decc88849ce9696965a9b00d2b7a0748489b4f8
sonadztux <dev.sonadztux@gmail.com>
2021-11-02 22:42:34 +0700

    Add migrator for face payment's model

--- backend/database/migrate.go
+++ backend/database/migrate.go
@@ -11,6 +11,9 @@ func Migrate(db *gorm.DB) {
 	db.AutoMigrate(&models.Feedback{})
 	db.AutoMigrate(&models.VisitorActivity{})
 	db.AutoMigrate(&models.Service{})
+	db.AutoMigrate(&models.FacePaymentAccount{})
+	db.AutoMigrate(&models.FacePaymentWallet{})
+	db.AutoMigrate(&models.FacePaymentTransaction{})
 }
 
 func DropTable(db *gorm.DB) {
@@ -19,4 +22,7 @@ func DropTable(db *gorm.DB) {
 	db.Migrator().DropTable(&models.Feedback{})
 	db.Migrator().DropTable(&models.VisitorActivity{})
 	db.Migrator().DropTable(&models.Service{})
+	db.Migrator().DropTable(&models.FacePaymentAccount{})
+	db.Migrator().DropTable(&models.FacePaymentWallet{})
+	db.Migrator().DropTable(&models.FacePaymentTransaction{})
 }
