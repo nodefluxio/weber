d223eb29328e61e52888e93e075eed2eb37e667d
krismp <kris.mp7777@gmail.com>
2021-11-09 10:22:52 +0700

    Intialize env load on init

--- backend/database/connect.go
+++ backend/database/connect.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -14,12 +13,6 @@ import (
 var DB *gorm.DB
 
 func InitDB() *gorm.DB {
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	dbHost := os.Getenv("DB_HOST")
 	dbUsername := os.Getenv("DB_USERNAME")
 	dbPassword := os.Getenv("DB_PASSWORD")
@@ -30,9 +23,9 @@ func InitDB() *gorm.DB {
 		"port=%s sslmode=disable TimeZone=Asia/Jakarta",
 		dbHost, dbUsername, dbPassword, dbName, dbPort)
 
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Panic("Error connecting to database: error = %v", err)
+		log.Panic(fmt.Printf("Error connecting to database: error = %v", err))
 	}
 
 	sqlDB, _ := DB.DB()
@@ -40,7 +33,8 @@ func InitDB() *gorm.DB {
 	sqlDB.SetMaxIdleConns(25)
 	sqlDB.SetConnMaxLifetime(5 * time.Minute)
 
-	fmt.Println("Database connection successfully established!")
+	log.Println("Database connection successfully established!")
+
 	return DB
 }
 
