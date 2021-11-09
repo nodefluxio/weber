551c90d3741a4ad93ca987cca0be2be629aa474e
Rizky Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2021-11-09 17:14:25 +0700

    add title to registration page

--- frontend/pages/request-demo/index.tsx
+++ frontend/pages/request-demo/index.tsx
@@ -1,8 +1,16 @@
 import { NextPage } from 'next'
+import Head from 'next/head'
 import { RequestDemoPage } from '../../app/components/templates/RequestDemoPage/RequestDemoPage'
 
 export const RequestDemo: NextPage = () => {
-  return <RequestDemoPage />
+  return (
+    <>
+      <Head>
+        <title>Request Nodeflux Demo Form</title>
+      </Head>
+      <RequestDemoPage />
+    </>
+  )
 }
 
 export default RequestDemo
