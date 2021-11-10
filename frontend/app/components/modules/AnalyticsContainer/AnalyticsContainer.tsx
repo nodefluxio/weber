cc022da00203bc2426126734f9d5e433254e4123
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-11-10 09:32:12 +0700

    add slug props to AnalyticsContainer,AnalyticsPage to work with new banner style

--- frontend/app/components/modules/AnalyticsContainer/AnalyticsContainer.tsx
+++ frontend/app/components/modules/AnalyticsContainer/AnalyticsContainer.tsx
@@ -10,6 +10,7 @@ type Props = {
   longDescription: string
   currentStep: number
   openModal: boolean
+  slug: string
   children: ReactNode
   onModalClose: () => void
 }
@@ -20,6 +21,7 @@ export const AnalyticsContainer = ({
   longDescription,
   currentStep,
   openModal,
+  slug,
   onModalClose,
   children
 }: Props) => {
@@ -29,6 +31,7 @@ export const AnalyticsContainer = ({
         <RequestDemoFormPopup />
       </Modal>
       <Banner
+        slug={slug}
         analyticsName={analyticsName}
         shortDescription={shortDescription}
         longDescription={longDescription}
