2960f7bdcb5e5954c4d6df1633fa706cccb874e5
William Fu <williamfu37@gmail.com>
2021-11-11 09:57:33 +0700

    moved a few component to ActivationForm to avoid memory leaks

--- frontend/app/components/modules/ActivationForm/ActivationForm.tsx
+++ frontend/app/components/modules/ActivationForm/ActivationForm.tsx
@@ -1,11 +1,18 @@
 import { PinInput } from '@/elements/PinInput/PinInput'
 import { Modal } from '@/elements/Modal/Modal'
 import { Button } from '../../elements/Button/Button'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styles from './ActivationForm.module.scss'
 import { Color } from '@/types/elements'
-import { PIN_DIGIT_LENGTH } from 'app/constants/amounts'
+import {
+  MAX_PAYMENT,
+  MIN_PAYMENT,
+  PIN_DIGIT_LENGTH
+} from 'app/constants/amounts'
 import { PaymentSetup } from '../PaymentSetup/PaymentSetup'
+import { activateAccount } from '@/api/paymentAPI'
+import { parseCookies } from 'nookies'
+import { Spinner } from '@/elements/Spinner/Spinner'
 
 type Props = {
   nextStep: Function
@@ -13,13 +20,55 @@ type Props = {
 
 export const ActivationForm = ({ nextStep }: Props) => {
   const [pinCode, setPinCode] = useState('')
+  const [payment, setPayment] = useState(MIN_PAYMENT)
+  const [warningMsg, setWarningMsg] = useState('')
   const [isPinCreated, setIsPinCreated] = useState(false)
   const [isModalShowed, setIsModalShowed] = useState(false)
+  const [isModalLoading, setIsModalLoading] = useState(false)
+
+  useEffect(() => {
+    if (payment < MIN_PAYMENT || payment > MAX_PAYMENT) {
+      setWarningMsg(
+        `*Enter value between IDR ${MIN_PAYMENT.toLocaleString()} and IDR ${MAX_PAYMENT.toLocaleString()}`
+      )
+    } else {
+      setWarningMsg('')
+    }
+  }, [payment])
+
+  const activate = async () => {
+    const { session_id } = parseCookies()
+    setIsModalLoading(true)
+    try {
+      const res = await activateAccount(session_id, pinCode, payment)
+      if (res.ok) {
+        setIsModalShowed(false)
+        nextStep()
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  }
 
   return (
     <>
       <Modal show={isModalShowed} onClose={() => setIsModalShowed(false)}>
-        <PaymentSetup onSuccess={nextStep} pinCode={pinCode}/>
+        {isModalLoading ? <Spinner /> : <PaymentSetup onChange={setPayment} />}
+        <span className={styles.warningMsg}>{warningMsg}</span>
+        <div className={styles.buttonWrapper}>
+          <Button
+            type="button"
+            color={Color.Primary}
+            disabled={
+              isModalLoading || payment < MIN_PAYMENT || payment > MAX_PAYMENT
+            }
+            onClick={() => {
+              activate()
+              setIsModalLoading(false)
+            }}>
+            Next
+          </Button>
+        </div>
       </Modal>
       <div className={styles.pinInputWrapper}>
         <div>
--- frontend/app/components/modules/PaymentSetup/PaymentSetup.tsx
+++ frontend/app/components/modules/PaymentSetup/PaymentSetup.tsx
@@ -6,22 +6,15 @@ import {
   TICK_INTERVAL,
   STEP_PAYMENT
 } from 'app/constants/amounts'
-import { parseCookies } from 'nookies'
-import { activateAccount } from '@/api/paymentAPI'
-import { Button } from '../../elements/Button/Button'
-import { Color } from '@/types/elements'
-import { Spinner } from '@/elements/Spinner/Spinner'
 
 type Props = {
-  onSuccess: Function
-  pinCode: string // 6-digit PIN code
+  onChange: Function
 }
 
-export const PaymentSetup = ({ onSuccess, pinCode }: Props) => {
+export const PaymentSetup = ({ onChange }: Props) => {
   const [value, setValue] = useState(MIN_PAYMENT) // value in float
   const [warningMsg, setWarningMsg] = useState('')
   const [fieldInput, setFieldInput] = useState('') // value displayed in text field
-  const [isLoading, setIsLoading] = useState(false)
 
   const ticks = Array.from(
     { length: Math.floor((MAX_PAYMENT - MIN_PAYMENT) / TICK_INTERVAL) + 1 },
@@ -44,86 +37,45 @@ export const PaymentSetup = ({ onSuccess, pinCode }: Props) => {
     setFieldInput((+e.target.value).toString())
   }
 
-  const activate = async () => {
-    const { session_id } = parseCookies()
-    setIsLoading(true)
-    try {
-      const res = await activateAccount(session_id, pinCode, value)
-      if (res.ok) {
-        onSuccess()
-      }
-    } catch (e) {
-      console.error(e)
-    } finally {
-      setIsLoading(false)
-    }
-  }
-
   useEffect(() => {
-    if (value < MIN_PAYMENT || value > MAX_PAYMENT) {
-      setWarningMsg(
-        `*Enter value between IDR ${MIN_PAYMENT.toLocaleString()} and IDR ${MAX_PAYMENT.toLocaleString()}`
-      )
-    } else {
-      setWarningMsg('')
-    }
+    onChange(value)
   }, [value])
 
   return (
     <div className={styles.modalPayment}>
-      {isLoading ? (
-        <>
-          <h2>Please wait...</h2>
-          <Spinner />
-        </>
-      ) : (
-        <>
-          <h2>Set up your minimum payment</h2>
-          <span className={styles.value}>{value.toLocaleString()}</span>
-          <p>
-            If the payment exceeds the minimum payment, you are required to
-            input pin
-          </p>
-          <div className={styles.sliderWrapper}>
-            <input
-              type="range"
-              min={MIN_PAYMENT}
-              max={MAX_PAYMENT}
-              step={STEP_PAYMENT}
-              list="tickmarks"
-              value={value}
-              className={styles.slider}
-              onChange={handleSlideChange}
-            />
-            <datalist id="tickmarks">
-              {ticks.map((v: number, i: number) => (
-                <option value={v} key={i} />
-              ))}
-            </datalist>
-          </div>
-          <span>You could type your nominal below</span>
-          <input
-            type="text"
-            value={fieldInput}
-            placeholder={'Enter nominal'}
-            maxLength={6}
-            onChange={handleInputChange}
-            className={styles.inputNominal}
-          />
-          <span className={styles.warningMsg}>{warningMsg}</span>
-          <div className={styles.buttonWrapper}>
-            <Button
-              type="button"
-              color={Color.Primary}
-              disabled={isLoading || value < MIN_PAYMENT || value > MAX_PAYMENT}
-              onClick={() => {
-                activate()
-              }}>
-              Next
-            </Button>
-          </div>
-        </>
-      )}
+      <h2>Set up your minimum payment</h2>
+      <span className={styles.value}>{value.toLocaleString()}</span>
+      <p>
+        If the payment exceeds the minimum payment, you are required to input
+        pin
+      </p>
+      <div className={styles.sliderWrapper}>
+        <input
+          type="range"
+          min={MIN_PAYMENT}
+          max={MAX_PAYMENT}
+          step={STEP_PAYMENT}
+          list="tickmarks"
+          value={value}
+          className={styles.slider}
+          onChange={handleSlideChange}
+        />
+        <datalist id="tickmarks">
+          {ticks.map((v: number, i: number) => (
+            <option value={v} key={i} />
+          ))}
+        </datalist>
+      </div>
+      <span>You could type your nominal below</span>
+      <input
+        type="text"
+        value={fieldInput}
+        placeholder={'Enter nominal'}
+        maxLength={6}
+        onChange={handleInputChange}
+        className={styles.inputNominal}
+      />
+      <span className={styles.warningMsg}>{warningMsg}</span>
     </div>
   )
 }
