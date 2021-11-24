7b9fdb0e31434675fef1a77d76495b776c3dc152
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-11-24 09:23:38 +0700

    change 'reach' to 'reached'

--- frontend/app/components/modules/PaymentPay/PaymentPay.tsx
+++ frontend/app/components/modules/PaymentPay/PaymentPay.tsx
@@ -134,7 +134,7 @@ export const PaymentPay = ({ sessionId, amount, afterPay }: Props) => {
       {step === 3 && (
         <div className={styles.pinInputWrapper}>
           <PinInput
-            message={'You’ve reach the limit, please input your pin'}
+            message={'You’ve reached the limit, please input your pin'}
             digits={PIN_DIGIT_LENGTH}
             onPinChange={setPinCode}
           />
