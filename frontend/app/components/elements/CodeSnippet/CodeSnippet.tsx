efab62fac839999218fac7fc7658fc3f384faf84
William Fu <williamfu37@gmail.com>
2021-11-27 14:31:15 +0700

    icon on copy button

--- frontend/app/components/elements/CodeSnippet/CodeSnippet.tsx
+++ frontend/app/components/elements/CodeSnippet/CodeSnippet.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { googlecode } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
+import Image from 'next/image'
 import styles from './CodeSnippet.module.scss'
 
 type Props = {
@@ -9,26 +10,23 @@ type Props = {
 }
 
 export const CodeSnippet = ({ code, lang }: Props) => {
-  const [isHovered, setIsHovered] = useState(false)
   const [isMessage, setIsMessage] = useState(false)
   const copyCode = () => {
     navigator.clipboard.writeText(code)
     setIsMessage(true)
     setTimeout(() => {
       setIsMessage(false)
-    }, 700)
+    }, 600)
   }
 
   return (
-    <div
-      className={styles.codeSnippetContainer}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}>
-      {isHovered && (
-        <button className={styles.receiptButtonCopy} onClick={() => copyCode()}>
-          {isMessage ? 'Copied!' : 'Copy to Clipboard'}
-        </button>
-      )}
+    <div className={styles.codeSnippetContainer}>
+      <button className={styles.receiptButtonCopy} onClick={() => copyCode()}>
+        <div className={styles.imagePadding}>
+          <Image src="/assets/icons/copy.svg" width={20} height={20} />
+        </div>
+      </button>
+      {isMessage && <div className={styles.receiptButtonCopy}>Copied!</div>}
       <SyntaxHighlighter language={lang} style={googlecode}>
         {code}
       </SyntaxHighlighter>
