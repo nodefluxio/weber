14654f754d97beb61498bf870299daecc5395f8c
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2021-11-30 01:29:37 +0700

    create object fit props

--- frontend/app/components/modules/ResponsiveImage/ResponsiveImage.tsx
+++ frontend/app/components/modules/ResponsiveImage/ResponsiveImage.tsx
@@ -5,12 +5,13 @@ type Props = {
   className?: string
   src: string
   alt: string
+  objectFit: NonNullable<JSX.IntrinsicElements['img']['style']>['objectFit']
 }
 
-export const ResponsiveImage = ({ className, src, alt }: Props) => {
+export const ResponsiveImage = ({ className, src, alt, objectFit }: Props) => {
   return (
     <div className={className || styles.responsiveImage}>
-      <Image src={src} alt={alt} layout="fill" objectFit="cover" />
+      <Image src={src} alt={alt} layout="fill" objectFit={objectFit} />
     </div>
   )
 }
