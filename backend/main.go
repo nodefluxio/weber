dda6e953c70dd4b2310777e406aea9477e598d45
bagasteguhimani <bagasteguhimani10@gmail.com>
2021-11-30 21:18:40 +0700

    put log severity LOG LEVEL to env

--- backend/main.go
+++ backend/main.go
@@ -22,7 +22,12 @@ func init() {
 	log.SetOutput(os.Stdout)
 
 	// Only log the warning severity or above.
-	log.SetLevel(log.WarnLevel)
+	logLevel, err := log.ParseLevel(os.Getenv("LOG_LEVEL"))
+	if err != nil {
+		logLevel = log.InfoLevel
+	}
+
+	log.SetLevel(logLevel)
 }
 
 func main() {
