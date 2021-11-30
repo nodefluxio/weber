68bef6e37b20e2242b42225d147eb21afc6442c5
bagasteguhimani <bagasteguhimani10@gmail.com>
2021-11-30 13:28:00 +0700

    add initiation logrus to main.go

--- backend/main.go
+++ backend/main.go
@@ -7,11 +7,22 @@ import (
 	"backend/utils"
 	"os"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/subosito/gotenv"
 )
 
 func init() {
 	gotenv.Load()
+
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.WarnLevel)
 }
 
 func main() {
