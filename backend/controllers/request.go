ea32f2c3867d8e336dae4bf389db2f53fee54977
sonadztux <dev.sonadztux@gmail.com>
2021-12-01 12:47:31 +0700

    add condition for request face occlusion attribute

--- backend/controllers/request.go
+++ backend/controllers/request.go
@@ -63,7 +63,8 @@ func RequestToServiceInnovation(ctx *gin.Context, service models.Service, inputD
 		string(additionalParams), strings.Join(requestData.Images, `", "`)))
 
 	if service.Slug == "face-occlusion-attribute" {
-		serviceData, err = ImplementFOAInnovation(postBody)
+		ImplementFOAInnovation(ctx, postBody)
+		return
 	} else {
 		serviceData, err = RequestToInnovationSync(postBody, service.Slug)
 	}
