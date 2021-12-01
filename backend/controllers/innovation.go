ff36b3dfbc7e83970f350cc5b381376fccfd73e9
sonadztux <dev.sonadztux@gmail.com>
2021-12-01 12:49:13 +0700

    update request FOA to use go concurrency

--- backend/controllers/innovation.go
+++ backend/controllers/innovation.go
@@ -8,9 +8,15 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 	"github.com/tidwall/gjson"
 )
+
+type foaServiceData struct {
+	FaceOcclusion	models.ServiceRequestResultData `json:"face_occlusion"`
+	FaceAttribute	models.ServiceRequestResultData `json:"face_attribute"`
+}
 
 func RequestToInnovationSync(postBody []byte, innovationSlug string) (models.ServiceRequestResultData, error) {
 	var err error
@@ -60,18 +66,121 @@ func RequestToInnovationSync(postBody []byte, innovationSlug string) (models.Ser
 	return data, nil
 }
 
-func ImplementFOAInnovation(postBody []byte) (models.ServiceRequestResultData, error) {
+func ImplementFOAInnovation(ctx *gin.Context, postBody []byte) {
 	// Request to Face Detection API
 	resultFaceDetection, err := RequestToInnovationSync(postBody, "face-detection")
 	if err != nil {
-		return resultFaceDetection, err
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"ok":      false,
+			"message": err.Error(),
+		})
+		return
 	}
 
-	resultFaceDetectionJson, _ := json.Marshal(resultFaceDetection)
+	resultFaceDetectionJson, err := json.Marshal(resultFaceDetection)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error":     	err,
+			"json_result": 	resultFaceDetectionJson,
+			"slug":      	"face-detection",
+		}).Error("error on marshaling json result")
+
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"ok":      false,
+			"message": err.Error(),
+		})
+		return
+	}
+
 	isFaceDetected := gjson.Get(string(resultFaceDetectionJson), "job.result.result.0.face_detection.face_detected").Bool()
 	if !isFaceDetected {
-		return resultFaceDetection, err
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"ok":      false,
+			"message": &resultFaceDetection.Message,
+		})
+		return
 	}
+
+	isMultipleFaceDetected := gjson.Get(string(resultFaceDetectionJson), "job.result.result.0.face_detection.multiple_faces_detected").Bool()
+	if isMultipleFaceDetected {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"ok":      false,
+			"message": &resultFaceDetection.Message,
+		})
+		return
+	}
+
+	// Get image base64 string for input Face Occlusion & Attribute
+	inputFaceOcclusion := gjson.Get(string(resultFaceDetectionJson), "job.result.result.0.face_detection.result.input_face_occlusion.0").String()
+	inputFaceAttribute := gjson.Get(string(resultFaceDetectionJson), "job.result.result.0.face_detection.result.input_face_attribute.0").String()
 	
-	return resultFaceDetection, nil
+	// Declare channels
+	resultFaceOcclusionChannel := make(chan models.ServiceRequestResultData)
+	resultFaceAttributeChannel := make(chan models.ServiceRequestResultData)
+	errorFaceOcclusionChannel := make(chan error)
+	errorFaceAttributeChannel := make(chan error)
+
+	// Create and execute goroutines
+	go func() {
+		resultFaceOcclusion, err := getResultFaceOcclusionAttribute(inputFaceOcclusion, "face-occlusion")
+		errorFaceOcclusionChannel <- err
+		resultFaceOcclusionChannel <- resultFaceOcclusion
+	}()
+	go func() {
+		resultFaceAttribute, err := getResultFaceOcclusionAttribute(inputFaceAttribute, "face-attribute")
+		errorFaceAttributeChannel <- err
+		resultFaceAttributeChannel <- resultFaceAttribute
+	}()
+	
+	var serviceData foaServiceData
+
+	// Share the results with channel - select
+	for i := 0; i < 4; i++ {
+		select {
+		case resultFaceOcclusion := <-resultFaceOcclusionChannel:
+			serviceData.FaceOcclusion = resultFaceOcclusion
+		case resultFaceAttribute := <-resultFaceAttributeChannel:
+			serviceData.FaceAttribute = resultFaceAttribute
+		case errorFaceOcclusion := <-errorFaceOcclusionChannel:
+			err = errorFaceOcclusion
+		case errorFaceAttribute := <-errorFaceAttributeChannel:
+			err = errorFaceAttribute
+		}
+	}
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error":        err,
+			"service_data": serviceData,
+		}).Error("error on implement face occlusion & attribute innovation")
+
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"ok":      false,
+			"message": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"ok":           true,
+		"message":      "Service demo request success",
+		"service_data": &serviceData,
+	})
+}
+
+func getResultFaceOcclusionAttribute(base64InputImage string, innovationSlug string) (models.ServiceRequestResultData, error) {
+	postBody := []byte(fmt.Sprintf(`{"images":  [ "%v" ]}`, base64InputImage))
+	resultFaceOcclusionAttribute, err := RequestToInnovationSync(postBody, innovationSlug)
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error":     err,
+			"post_body": postBody,
+			"slug":      innovationSlug,
+		}).Error("error on request innovation service")
+
+		return resultFaceOcclusionAttribute, err
+	}
+
+	return resultFaceOcclusionAttribute, nil
 }
