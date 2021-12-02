419408f56b9b324e076250f4f17638456a4a9665
bagasteguhimani <bagasteguhimani10@gmail.com>
2021-12-02 14:18:49 +0700

    Update log message, Add INFO logs, edit minimal input in Visitor

--- backend/models/visitor.go
+++ backend/models/visitor.go
@@ -9,50 +9,77 @@ import (
 type Visitor struct {
 	SessionID string    `gorm:"primary_key" json:"session_id"`
 	Email     string    `json:"email" validate:"required,email"`
-	FullName  string    `json:"full_name" validate:"required,min=2,max=255"`
-	Company   string    `json:"company" validate:"required,min=2,max=255"`
-	JobTitle  string    `json:"job_title" validate:"required,min=2,max=255"`
+	FullName  string    `json:"full_name" validate:"required,min=5,max=255"`
+	Company   string    `json:"company" validate:"required,min=5,max=255"`
+	JobTitle  string    `json:"job_title" validate:"required,min=5,max=255"`
 	Industry  string    `json:"industry" validate:"required,min=2,max=255"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
 func (m *Model) CreateVisitor(Visitor *Visitor) (err error) {
+
+	log.WithFields(log.Fields{
+		"data": Visitor,
+	}).Info("[MODEL] create visitor start...")
+
 	err = m.DBConn.Create(Visitor).Error
 	if err != nil {
 		log.WithFields(log.Fields{
 			"error": err,
 			"data":  Visitor,
-		}).Error("error on create visitor!")
+		}).Error("[MODEL] error on create visitor!")
 
 		return err
 	}
+	log.WithFields(log.Fields{
+		"data": Visitor,
+	}).Info("[MODEL] success on create visitor!")
+
 	return nil
 }
 
 func (m *Model) GetVisitors(Visitor *[]Visitor) (err error) {
+
+	log.WithFields(log.Fields{
+		"data": Visitor,
+	}).Info("[MODEL] get visitors start...")
+
 	err = m.DBConn.Find(Visitor).Error
 	if err != nil {
 		log.WithFields(log.Fields{
 			"error": err,
 			"data":  Visitor,
-		}).Error("error on get visitors!")
+		}).Error("[MODEL] error on get visitors!")
 
 		return err
 	}
+	log.WithFields(log.Fields{
+		"data": Visitor,
+	}).Info("[MODEL] success on get visitors!")
+
 	return nil
 }
 
 func (m *Model) GetVisitor(Visitor *Visitor, id string) (err error) {
+
+	log.WithFields(log.Fields{
+		"data": Visitor,
+		"id":   id,
+	}).Info("[MODEL] get visitor start...")
+
 	err = m.DBConn.Where("session_id = ?", id).First(Visitor).Error
 	if err != nil {
 		log.WithFields(log.Fields{
 			"session_id": id,
 			"error":      err,
-		}).Error("error get visitor based on session_id!")
+		}).Error("[MODEL] error get visitor based on session_id!")
 
 		return err
 	}
+	log.WithFields(log.Fields{
+		"data": Visitor,
+	}).Info("[MODEL] success on get visitor")
 	return nil
 }
 
