da0926c1ee51e730f91bf4ba0d930191f16b4a4e
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-12-03 09:56:32 +0700

    add FaceOcclusionAttributeResponse in responses.ts

--- frontend/app/types/responses.ts
+++ frontend/app/types/responses.ts
@@ -5,6 +5,10 @@ import {
   PaymentAccountInfo,
   Service
 } from './elements'
+import {
+  FaceAttributeResultResponse,
+  FaceOcclusionResultResponse
+} from './results'
 
 export type StandardResponse = {
   message: string
@@ -149,5 +153,12 @@ export type InnovationResponse<T> = {
     }
     message: string
     ok: boolean
+  }
+}
+
+export interface FaceOcclusionAttributeResponse extends StandardResponse {
+  service_data: {
+    face_occlusion: NodefluxCloudResponse<FaceOcclusionResultResponse>
+    face_attribute: NodefluxCloudResponse<FaceAttributeResultResponse>
   }
 }
