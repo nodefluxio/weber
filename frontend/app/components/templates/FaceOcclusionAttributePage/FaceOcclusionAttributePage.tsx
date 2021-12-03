3f15aa175b87fe4f8e31658b28e2f453f4cb7851
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-12-03 09:58:43 +0700

    add FaceOcclusionAttributePage

---
+++ frontend/app/components/templates/FaceOcclusionAttributePage/FaceOcclusionAttributePage.tsx
@@ -0,0 +1,127 @@
+import { AnalyticsPage } from '@/templates/AnalyticsPage/AnalyticsPage'
+import { useEffect, useState } from 'react'
+import { Tabs } from '@/modules/Tabs/Tabs'
+import { Tab } from '@/elements/Tab/Tab'
+import { postFaceOcclusionAttribute } from '@/api/innovationsAPI'
+import { ProgressBar } from '@/elements/ProgressBar/ProgressBar'
+import {
+  Color,
+  FaceAttributeAdditionalLabel,
+  FaceAttributeLabel,
+  FaceOcclusionLabel
+} from '@/types/elements'
+import {
+  FaceAttributeResultResponse,
+  FaceOcclusionResultResponse
+} from '@/types/results'
+import { NodefluxCloudResponse } from '@/types/responses'
+import {
+  FACE_ATTRIBUTE_LABEL,
+  FACE_OCCLUSION_LABEL
+} from 'app/constants/constant'
+import styles from './FaceOcclusionAttributePage.module.scss'
+
+type Props = {
+  id: number
+  name: string
+  slug: string
+  long_description: string
+}
+type Result = {
+  face_occlusion: NodefluxCloudResponse<FaceOcclusionResultResponse>
+  face_attribute: NodefluxCloudResponse<FaceAttributeResultResponse>
+}
+
+export const FaceOcclusionAttributePage = ({
+  id,
+  name,
+  slug,
+  long_description
+}: Props) => {
+  const [result, setResult] = useState<Result>()
+  const [faceOcclusionDetections, setFaceOcclusionDetections] =
+    useState<Map<FaceOcclusionLabel, number>>()
+  const [faceAttributeDetections, setFaceAttributeDetections] =
+    useState<Map<FaceAttributeLabel | FaceAttributeAdditionalLabel, number>>()
+  const [faceOcclusionMessage, setFaceOcclusionMessage] = useState<string>()
+  const [faceAttributeMessage, setFaceAttributeMessage] = useState<string>()
+
+  useEffect(() => {
+    const faceOcclusionMap = new Map<FaceOcclusionLabel, number>()
+    const faceAttributeMap = new Map<
+      FaceAttributeLabel | FaceAttributeAdditionalLabel,
+      number
+    >()
+    if (result?.face_occlusion.ok) {
+      result.face_occlusion.job.result.result[0].face_occlusion.detections.forEach(
+        (detection) =>
+          faceOcclusionMap.set(detection.label, detection.confidence)
+      )
+      setFaceOcclusionDetections(faceOcclusionMap)
+    } else setFaceOcclusionMessage(result?.face_occlusion.message)
+
+    if (result?.face_attribute.ok) {
+      result.face_attribute.job.result.result[0].face_attribute.occlusion_object.forEach(
+        (object) => faceAttributeMap.set(object.label, object.confidence * 100)
+      )
+      result.face_attribute.job.result.result[0].face_attribute.additional_info_object.forEach(
+        (object) => faceAttributeMap.set(object.label, object.confidence * 100)
+      )
+      setFaceAttributeDetections(faceAttributeMap)
+    } else setFaceAttributeMessage(result?.face_attribute.message)
+  }, [result])
+
+  return (
+    <AnalyticsPage
+      analyticsName={name}
+      longDescription={long_description}
+      examples={[
+        `/assets/images/innovations/${slug}/example1.jpg`,
+        `/assets/images/innovations/${slug}/example2.jpg`,
+        `/assets/images/innovations/${slug}/example3.jpg`
+      ]}
+      serviceID={id}
+      slug={slug}
+      handlePost={(session_id, photo) =>
+        postFaceOcclusionAttribute(id, session_id, photo)
+      }
+      handleResult={(res) => setResult(res)}>
+      {result && (
+        <div className={styles.FOAResult}>
+          <Tabs>
+            <Tab className={styles.tab} title="Face Occlusion">
+              {faceOcclusionDetections ? (
+                FACE_OCCLUSION_LABEL.map((label, idx) => (
+                  <div className={styles.content} key={idx}>
+                    <p>{label}</p>
+                    <ProgressBar
+                      bgColor={Color.Primary}
+                      completed={faceOcclusionDetections.get(label) || 0}
+                    />
+                  </div>
+                ))
+              ) : (
+                <p>{faceOcclusionMessage}</p>
+              )}
+            </Tab>
+            <Tab className={styles.tab} title="Face Attribute">
+              {faceAttributeDetections ? (
+                FACE_ATTRIBUTE_LABEL.map((label, idx) => (
+                  <div className={styles.content} key={idx}>
+                    <p>{label}</p>
+                    <ProgressBar
+                      bgColor={Color.Primary}
+                      completed={faceAttributeDetections.get(label) || 0}
+                    />
+                  </div>
+                ))
+              ) : (
+                <p>{faceAttributeMessage}</p>
+              )}
+            </Tab>
+          </Tabs>
+        </div>
+      )}
+    </AnalyticsPage>
+  )
+}
