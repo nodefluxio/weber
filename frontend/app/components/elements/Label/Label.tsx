1537a1e460374fb22f5a128d07ba737f06ff509d
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-12-12 12:57:46 +0700

    update Label to have children
    Co-authored-by: William Fu <williammfu@users.noreply.github.com>

--- frontend/app/components/elements/Label/Label.tsx
+++ frontend/app/components/elements/Label/Label.tsx
@@ -1,16 +1,19 @@
 import { ErrorMessage } from '@hookform/error-message'
+import { ReactNode } from 'react'
 import { DeepMap } from 'react-hook-form'
 import styles from './Label.module.scss'
 type Props = {
   id: string
-  label: string
+  label?: string
+  children?: ReactNode
   errors?: DeepMap<any, any>
 }
 
-export const Label = ({ id, label, errors }: Props) => {
+export const Label = ({ id, label, children, errors }: Props) => {
   return (
     <label className={styles.label} htmlFor={id}>
       {label}
+      {children}
       {errors ? (
         <ErrorMessage
           className={styles.errorMessage}
