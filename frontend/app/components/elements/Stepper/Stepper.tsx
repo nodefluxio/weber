53d9b1cad51b95dccc102dbb9564414736932ba2
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-12-18 10:24:06 +0700

    tailwindting Stepper (not finisihed)

--- frontend/app/components/elements/Stepper/Stepper.tsx
+++ frontend/app/components/elements/Stepper/Stepper.tsx
@@ -3,23 +3,27 @@ import styles from './Stepper.module.scss'
 type Props = {
   steps: string[]
   activeStep: number
+  className?: string
 }
 
-export const Stepper = ({ steps, activeStep }: Props) => {
+export const Stepper = ({ steps, activeStep, className }: Props) => {
   return (
-    <ol className={styles.stepper}>
+    <ol
+      className={`flex w-full mx-auto justify-center overflow-hidden ${className}`}>
       {steps.map((step: string, idx: number) => (
+        // stepper item
         <li
-          className={`${styles.stepperItem} ${
-            idx + 1 === activeStep ? styles.current : ''
-          } ${idx + 1 === activeStep + 1 ? styles.next : ''}`}
+          className={`flex flex-col text-center ${styles.stepperItem} ${
+            idx + 1 === activeStep ? 'flex' : ''
+          } ${idx + 1 === activeStep + 1 ? 'flex' : ''}`}
           key={idx}>
+          {/* stepper bulletin */}
           <div
-            className={`${styles.stepperBulletin} ${
-              idx + 1 === activeStep ? styles.active : styles.inactive
+            className={`mx-32 sm:mx-[7vw] rounded-full w-8 h-8 ${
+              idx + 1 === activeStep ? 'bg-primary-500' : 'bg-primary-200'
             }`}
           />
-          <p>{step}</p>
+          <p className="font-medium">{step}</p>
         </li>
       ))}
     </ol>
