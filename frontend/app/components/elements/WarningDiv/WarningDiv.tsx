a1af1b235658777efeda40ad0d23e35b14c8bc2f
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-12-19 21:00:13 +0700

    tailwindting WarningDiv

--- frontend/app/components/elements/WarningDiv/WarningDiv.tsx
+++ frontend/app/components/elements/WarningDiv/WarningDiv.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import styles from './WarningDiv.module.scss'
 
 type Props = {
   message: string
@@ -10,19 +9,20 @@ export const WarningDiv = ({ message, className }: Props) => {
   return (
     <div className={className}>
       <div
-        className={`${styles.warningDivWrapper} ${
-          message && styles.warningDivWrapperActive
+        className={`text-yellow-700 flex flex-col sm:flex-row items-center justify-center py-3 px-3 ${
+          message && 'bg-yellow-200 border-yellow-700 border rounded'
         }`}>
         {message && (
-          <div className={styles.warningImageWrapper}>
+          <div className="flex-shrink-0 flex items-center">
             <Image
               src={'/assets/icons/error-icon.svg'}
               width={32}
               height={32}
+              alt="alert symbol"
             />
           </div>
         )}
-        <span className={styles.messageSpan}>{message}</span>
+        <span className="mt-3 sm:mt-0 sm:ml-3 text-sm">{message}</span>
       </div>
     </div>
   )
