84d7680188643147285357a657dbaf02bf581401
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-12-21 09:14:00 +0700

    tailwindting AnalyticsResult wrapper

--- frontend/app/components/modules/AnalyticsResultWrapper/AnayticsResultWrapper.tsx
+++ frontend/app/components/modules/AnalyticsResultWrapper/AnayticsResultWrapper.tsx
@@ -1,32 +1,34 @@
-import Image from 'next/image'
 import { ReactNode } from 'react'
-
-import styles from './AnalyticsResultWrapper.module.scss'
+import { ResponsiveImage } from '../ResponsiveImage/ResponsiveImage'
 
 type Props = {
   imageBase64: string
   children: ReactNode
   handleTryAgain: () => void
+  className?: string
 }
 
-export const AnalyticsResultWrapper = ({ imageBase64, children }: Props) => {
+export const AnalyticsResultWrapper = ({
+  imageBase64,
+  children,
+  className
+}: Props) => {
   return (
-    <>
-      <div className={styles.result}>
-        <div className={styles.resultFlex}>
-          <div className={styles.resultImage}>
-            {imageBase64 && (
-              <Image src={imageBase64} layout="fill" objectFit="contain" />
-            )}
-          </div>
-          <div className={styles.resultInfo}>
-            <div className={styles.resultFlexInfo}>
-              <h2>Results</h2>
-              {children}
-            </div>
-          </div>
+    <div className={`w-full ${className}`}>
+      <div className="flex flex-col md:flex-row justify-center items-center">
+        {imageBase64 && (
+          <ResponsiveImage
+            className="h-48 w-full max-w-xs md:max-w-lg md:w-1/2 md:mr-8 md:h-80"
+            src={imageBase64}
+            objectFit="cover"
+            alt="inputed image"
+          />
+        )}
+        <div className="mt-6 w-full md:w-1/2 md:self-start md:mt-0 max-w-lg">
+          <h3 className="font-medium text-lg mb-2">Results</h3>
+          {children}
         </div>
       </div>
-    </>
+    </div>
   )
 }
