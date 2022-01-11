717524f69ed2fe2f13cad50180789e5427486b78
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2022-01-11 15:31:25 +0700

    add SolutionCard component

---
+++ frontend/app/components/modules/SolutionCard/SolutionCard.tsx
@@ -0,0 +1,43 @@
+import { CardFull } from '@/modules/CardFull/CardFull'
+import { ResponsiveImage } from '@/modules/ResponsiveImage/ResponsiveImage'
+import { Service } from '@/types/elements'
+import { HTMLAttributeAnchorTarget } from 'react'
+
+type Props = {
+  solution: Service
+  isExternal?: boolean
+  isPopup?: boolean
+  target?: HTMLAttributeAnchorTarget
+  handleClick?: () => void
+}
+
+export const SolutionCard = ({
+  solution,
+  isExternal,
+  isPopup,
+  target,
+  handleClick
+}: Props) => {
+  return (
+    <CardFull
+      href={`/solutions/${solution.slug}`}
+      isPopUp={isPopup}
+      isExternal={isExternal}
+      target={target}
+      onClick={() => handleClick}>
+      <div className="flex flex-col w-full relative">
+        <ResponsiveImage
+          className="w-full h-[220px] xl:h-[300px] 2xl:h-[380px] mb-2"
+          alt={`image of ${solution.name}`}
+          src={`/assets/images/solutions/${solution.thumbnail}`}
+          objectFit="cover"
+          objectPosition="top"
+        />
+        <div className="px-6 py-4 min-h-[250px] sm:min-h-[230px] md:min-h-[210px] xl:min-h-[180px]">
+          <h4 className="mb-2 text-lg">{solution.name}</h4>
+          <p className="font-serif ">{solution.short_description}</p>
+        </div>
+      </div>
+    </CardFull>
+  )
+}
