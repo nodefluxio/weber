e7104a8e95f6c3807f21165d01b3d656999bbc33
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2022-01-27 13:40:12 +0700

    modify postFeedback in feedbackApi
    Co-authored-by: William Fu <williammfu@users.noreply.github.com>
    Co-authored-by: Rizky Muhammad Baihaqy <rizkybaihaqy@users.noreply.github.com>

--- frontend/app/api/feedbackAPI.ts
+++ frontend/app/api/feedbackAPI.ts
@@ -5,24 +5,21 @@ import { StandardResponse } from '../types/responses'
 type ReviewReqBody = {
   id: number
   session_id: string
-  rating?: number
+  rating: number
   comment: string
-  jobId?: string
 }
 
 export const postFeedback = async ({
   id,
   session_id,
   rating,
-  comment,
-  jobId
+  comment
 }: ReviewReqBody): Promise<StandardResponse | undefined> => {
   try {
     const res = await axios.post<StandardResponse>(`/feedback/${id}`, {
       session_id,
       rating,
-      comment,
-      jobId
+      comment
     })
     if (res.data.ok) {
       return res.data
