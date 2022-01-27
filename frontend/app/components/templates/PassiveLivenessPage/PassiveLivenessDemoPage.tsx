1a32210f8763ebde7683eb9ea19485b9d123b2da
William Fu <williamfu37@gmail.com>
2022-01-27 08:49:11 +0700

    add liveness feedback to passive template page

--- frontend/app/components/templates/PassiveLivenessPage/PassiveLivenessDemoPage.tsx
+++ frontend/app/components/templates/PassiveLivenessPage/PassiveLivenessDemoPage.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/elements/Button/Button'
 import { Modal } from '@/elements/Modal/Modal'
 import { Stepper } from '@/elements/Stepper/Stepper'
 import { Banner } from '@/modules/Banner/Banner'
+import { LivenessReview } from '@/modules/LivenessReview/LivenessReview'
 import { RequestDemoFormPopup } from '@/modules/RequestDemoFormPopup/RequestDemoFormModal'
 import { Color } from '@/types/elements'
 import { parseCookies } from 'nookies'
@@ -48,7 +49,7 @@ export const PassiveLivenessDemoPage = ({ name, longDesc }: Props) => {
       <div className="container mx-auto w-[90%] md:w-4/5 min-h-[75vh] py-8 flex flex-col justify-center items-center text-center">
         {currentStep === 1 && (
           <div className="flex flex-col items-center">
-            <h3 className="pb-6 text-2xl">Welcome to e-KYC Demo</h3>
+            <h3 className="pb-6 text-2xl">Welcome to Liveness Demo App</h3>
             <div className="bg-secondary-500 w-full md:w-7/12 border border-solid rounded-lg my-8 h-min">
               <p className="m-4 text-lg">
                 Please access this demo via <strong>smartphone</strong> or any
@@ -65,6 +66,21 @@ export const PassiveLivenessDemoPage = ({ name, longDesc }: Props) => {
               }}>
               Start
             </Button>
+          </div>
+        )}
+        {currentStep === 4 && (
+          <div className="flex flex-col">
+            <h2 className="text-2xl font-bold">
+              Thank you for using the Liveness Demo App
+            </h2>
+            <span className="text-xl font-semibold mt-10">
+              Is our liveness prediction true?
+            </span>
+            <LivenessReview
+              id={0}
+              onChosen={() => console.log('Clicked!')}
+              className="my-4"
+            />
           </div>
         )}
       </div>
