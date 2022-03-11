8418640c18959ef84912917dbfd9f7ea7f263f41
gregoriusjimmy <gregoriusjimmy@gmail.com>
2022-03-11 13:14:56 +0700

    fix storybook cannot find swiper/css file

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,6 +3,8 @@ import { Default } from '../app/components/layouts/Default/Default'
 import { SmoothScroll } from '@/modules/SmoothScroll/SmoothScroll'
 import '../app/styles/global.css'
 import '../app/utils/axios/axiosConfig'
+import 'swiper/css'
+import 'swiper/css/pagination'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
