cd23739aa7651017bf3db7d6eaeeb605ac38007d
valdmir <vald.karate@gmail.com>
2022-06-30 20:00:17 +0800

    remvoe innovation page

--- frontend/app/components/templates/HomePage/HomePage.tsx
+++ frontend/app/components/templates/HomePage/HomePage.tsx
@@ -325,7 +325,7 @@ export const HomePage = ({
             ))}
           </ul>
         </section>
-        <Intersection
+        {/* <Intersection
           imgSrc="/assets/images/innovations-01.png"
           imgWidth={443}
           imgHeight={425}
@@ -364,7 +364,7 @@ export const HomePage = ({
               </div>
             ))}
           </div>
-        </section>
+        </section> */}
       </div>
     </>
   )
